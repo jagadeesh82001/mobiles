292ee4f22398fa71a5a69c7550211dc4f3dce8f7
jagadeesh82001 <145035048+jagadeesh82001@users.noreply.github.com>
2023-10-08 11:58:16 +0530

    Add files via upload
    mobiles project

---
+++ mobiles/src/main/java/com/example/mobiles/MobilesApplication.java
@@ -0,0 +1,13 @@
+package com.example.mobiles;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MobilesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MobilesApplication.class, args);
+	}
+
+}
---
+++ mobiles/src/main/java/com/example/mobiles/entities/Mobiles.java
@@ -0,0 +1,5 @@
+package com.example.mobiles.entities;
+
+public class Mobiles {
+
+}
