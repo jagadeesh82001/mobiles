b6f6c2d9b1121302743c75dda48f3a105617489f
jagadeesh82001 <jagadeeshgopi001@gmail.com>
2023-10-08 12:24:19 +0530

    Added service layer

---
+++ mobiles/src/main/java/com/example/mobiles/MobileService.java
@@ -0,0 +1,12 @@
+package com.example.mobiles;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class MobileService {
+
+    public String mobileTracker(){
+      String  mobile="6309739123";
+        return null;
+    }
+}
